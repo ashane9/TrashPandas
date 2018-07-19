f905993483c18fbbd8767e0c8aac3ff64f2d93d4
Andrew Shane <shanea1@nationwide.com>
2018-07-19 16:38:50 -0400

    added methods for menu and card details as well as view files

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,2 +1,14 @@
 class MobileController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def menu
+    rtc_number = params[:number]
+    unless rtc_number.nil?
+      redirect_to mobile_card_path(rtc_number)
+    end
+  end
+
+  def card_details
+    @card = Card.find(params[:number])
+  end
 end
--- app/controllers/rtc_controller.rb
+++ app/controllers/rtc_controller.rb
@@ -1,2 +1,12 @@
 class RtcController < ApplicationController
+  def menu
+    rtc_number = params[:number]
+    unless rtc_number.nil?
+      redirect_to card_path(rtc_number)
+    end
+  end
+
+  def card_details
+    @card = Card.find(params[:number])
+  end
 end
