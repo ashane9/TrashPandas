95c22a81637374b014b75712d1eca6dac612a6ea
Andrew Shane <shanea1@nationwide.com>
2018-07-19 17:19:32 -0400

    added view content and controller for mobile

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -11,4 +11,20 @@ class MobileController < ApplicationController
   def card_details
     @card = Card.find(params[:number])
   end
+
+  def update
+    rtc_number = params[:number]
+    @card = Card.find(rtc_number)
+    if @card.update_attributes(card_params)
+      flash[:success] = 'Changes were successfully saved.'
+      redirect_to mobile_card_path(rtc_number)
+    else
+      render 'card_details'
+    end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:summary, :card_type, :status, :points, :owner, :blocked, :iteration)
+  end
 end
