040d5b252a9befcb12611e0e21c1f60e1352a3ec
Andrew Shane <shanea1@nationwide.com>
2018-07-19 16:30:51 -0400

    added in get and post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get '/menu' => 'mobile#menu'
+    get '/rtc' => 'rtc#menu'
+    post '/menu' => 'mobile#menu', as: :post_mobile_card
+    post '/rtc' => 'rtc#menu', as: :post_card
 end
