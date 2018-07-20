b2a4b4b3da103ed170dfc094655fa17c09270bf4
Andrew Shane <shanea1@nationwide.com>
2018-07-20 03:31:45 -0400

    added update for rtc controller

--- app/controllers/rtc_controller.rb
+++ app/controllers/rtc_controller.rb
@@ -9,4 +9,23 @@ class RtcController < ApplicationController
   def card_details
     @card = Card.find(params[:number])
   end
+
+  def update
+    rtc_number = params[:number]
+    @card = Card.find(rtc_number)
+    prev_status = @card.status
+    if @card.update_attributes(card_params)
+      flash[:success] = 'Changes were successfully saved.'
+      @card = Card.find(rtc_number)
+      CardDoneMailer.done(@card).deliver if @card.status == 'Set Done' and prev_status != 'Set Done'
+      redirect_to card_path(rtc_number)
+    else
+      render 'card_details'
+    end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:description, :comment, :summary, :card_type, :status, :points, :owner, :blocked, :iteration)
+  end
 end
